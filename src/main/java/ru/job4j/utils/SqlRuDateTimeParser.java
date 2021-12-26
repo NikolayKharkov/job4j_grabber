8348e22f02a40dccfe582e6f51ddb5f569fb49ab
NikolayKharkov <vtiko564@gmail.com>
2021-12-26 18:58:58 +0300

    2.1. Преобразование даты [#289476 #244347]

--- src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
@@ -6,9 +6,6 @@ import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
 
-    private static final DateTimeFormatter FORMATTER =
-            DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm");
-
     private static final Map<String, String> MONTHS = Map.ofEntries(
             Map.entry("янв", "01"),
             Map.entry("фев", "02"),
@@ -30,32 +27,27 @@ public class SqlRuDateTimeParser implements DateTimeParser {
         String time = parse.split(",")[1].trim();
         switch (date) {
             case "вчера":
-                LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
-                String yesterdayStr = String.format("%s-%s-%s %s",
-                        yesterday.getYear(),
-                        yesterday.getMonthValue(),
-                        yesterday.getDayOfMonth(),
-                        time);
-                result = LocalDateTime.parse(yesterdayStr, FORMATTER);
+                result = returnDateTime(LocalDateTime.now().minusDays(1), time);
                 break;
             case "сегодня":
-                LocalDateTime today = LocalDateTime.now();
-                String todayStr = String.format("%s-%s-%s %s",
-                        today.getYear(),
-                        today.getMonthValue(),
-                        today.getDayOfMonth(),
-                        time);
-                result = LocalDateTime.parse(todayStr, FORMATTER);
+                result = returnDateTime(LocalDateTime.now(), time);
                 break;
             default:
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm");
                 String[] tmp = date.split(" ");
                 String dateStr = String.format("%d-%s-%s %s",
                         (Integer.parseInt(tmp[2]) + 2000),
                         MONTHS.get(tmp[1]),
                         tmp[0],
                         time);
-                result = LocalDateTime.parse(dateStr, FORMATTER);
+                result = LocalDateTime.parse(dateStr, formatter);
         }
         return result;
+    }
+
+    private static LocalDateTime returnDateTime(LocalDateTime date, String time) {
+        return date
+                .withHour(Integer.parseInt(time.split(":")[0]))
+                .withMinute(Integer.parseInt(time.split(":")[1]));
     }
 }
