5b16bf8b747b38054526f1b28c2cba1fe3dc5671
NikolayKharkov <vtiko564@gmail.com>
2021-12-26 19:58:55 +0300

    2.1. Преобразование даты [#289476 #244347]

--- src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
@@ -1,53 +1,53 @@
 package ru.job4j.utils;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
 
-    private static final Map<String, String> MONTHS = Map.ofEntries(
-            Map.entry("янв", "01"),
-            Map.entry("фев", "02"),
-            Map.entry("мар", "03"),
-            Map.entry("апр", "04"),
-            Map.entry("май", "05"),
-            Map.entry("июн", "06"),
-            Map.entry("июл", "07"),
-            Map.entry("авг", "08"),
-            Map.entry("сен", "09"),
-            Map.entry("окт", "10"),
-            Map.entry("ноя", "11"),
-            Map.entry("дек", "12"));
+    private static final Map<String, Integer> MONTHS = Map.ofEntries(
+            Map.entry("янв", 1),
+            Map.entry("фев", 2),
+            Map.entry("мар", 3),
+            Map.entry("апр", 4),
+            Map.entry("май", 5),
+            Map.entry("июн", 6),
+            Map.entry("июл", 7),
+            Map.entry("авг", 8),
+            Map.entry("сен", 9),
+            Map.entry("окт", 10),
+            Map.entry("ноя", 11),
+            Map.entry("дек", 12));
 
     @Override
     public LocalDateTime parse(String parse) {
-        LocalDateTime result = null;
+        LocalDateTime result;
         String date = parse.split(",")[0].trim();
         String time = parse.split(",")[1].trim();
         switch (date) {
             case "вчера":
-                result = returnDateTime(LocalDateTime.now().minusDays(1), time);
+                result = returnDateTime(time).minusDays(1);
                 break;
             case "сегодня":
-                result = returnDateTime(LocalDateTime.now(), time);
+                result = returnDateTime(time);
                 break;
             default:
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm");
                 String[] tmp = date.split(" ");
-                String dateStr = String.format("%d-%s-%s %s",
-                        (Integer.parseInt(tmp[2]) + 2000),
+                result = LocalDateTime.of(
+                        Integer.parseInt(tmp[2]) + 2000,
                         MONTHS.get(tmp[1]),
-                        tmp[0],
-                        time);
-                result = LocalDateTime.parse(dateStr, formatter);
+                        Integer.parseInt(tmp[0]),
+                        Integer.parseInt(time.split(":")[0]),
+                        Integer.parseInt(time.split(":")[1]));
         }
         return result;
     }
 
-    private static LocalDateTime returnDateTime(LocalDateTime date, String time) {
-        return date
+    private static LocalDateTime returnDateTime(String time) {
+        return LocalDateTime.now()
                 .withHour(Integer.parseInt(time.split(":")[0]))
-                .withMinute(Integer.parseInt(time.split(":")[1]));
+                .withMinute(Integer.parseInt(time.split(":")[1]))
+                .truncatedTo(ChronoUnit.MINUTES);
     }
 }
