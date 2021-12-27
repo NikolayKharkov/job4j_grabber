0b32b93ffa9389a5b0c1cd9a423eaf3e8eda7e54
NikolayKharkov <vtiko564@gmail.com>
2021-12-27 21:22:18 +0300

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -23,14 +23,25 @@ public class PsqlStore implements Store, AutoCloseable {
 
     @Override
     public void save(Post post) {
-        try (PreparedStatement statement =
+        try (PreparedStatement preStatement =
                      cnn.prepareStatement(
-                             "insert into posts(name, text, link, created) values (?, ?, ?, ?)")) {
-            statement.setString(1, post.getTitle());
-            statement.setString(2, post.getDescription());
-            statement.setString(3, post.getLink());
-            statement.setTimestamp(4, Timestamp.valueOf(post.getLocalDateTime()));
-            statement.execute();
+                             "select * from posts where link = ?")) {
+            preStatement.setString(1, post.getLink());
+            preStatement.execute();
+            try (ResultSet checkUnique = preStatement.executeQuery()) {
+                if (!checkUnique.isBeforeFirst()) {
+                    try (PreparedStatement statement =
+                                 cnn.prepareStatement(
+                                         "insert into posts(name, text, link, created)"
+                                                 + " values (?, ?, ?, ?)")) {
+                        statement.setString(1, post.getTitle());
+                        statement.setString(2, post.getDescription());
+                        statement.setString(3, post.getLink());
+                        statement.setTimestamp(4, Timestamp.valueOf(post.getLocalDateTime()));
+                        statement.execute();
+                    }
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
